052155939757a1ba9b4c50ccfb243c4656b135ea
SHAIK-GHOUSE1 <shaikghousemohammed045@gmail.com>
2026-05-26 21:12:11 +0530

    change in ride-sharing-service file

--- ride-sharing-service.py
+++ ride-sharing-service.py
@@ -5,3 +5,16 @@ class RideSharingServiceApp:
     def __int__(self):
         self.drivers:List[Driver]=[]
         self.passengers:List[Passenger]=[]
+    # Method to add a Driver
+    def add_driver(self,driver:Driver):
+        self.drivers.append(driver)
+    # Method to add a passenger
+    def add_passenger(self,passenger:Passenger):
+        self.passengers.append(passenger)
+    # Method to book ride
+    def bookRide(self,passenger:Passenger,distance):
+        if len(self.drivers)==0:
+            print(f"No driver avaliable for {passenger.name}")
+            return
+        
+        
