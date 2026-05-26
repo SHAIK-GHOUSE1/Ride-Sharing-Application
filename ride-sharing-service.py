953d4b1ec2c833920333c8c04c6020e5b7a38dc8
SHAIK-GHOUSE1 <shaikghousemohammed045@gmail.com>
2026-05-26 21:00:08 +0530

    first commit to ride-sharing

---
+++ ride-sharing-service.py
@@ -0,0 +1,7 @@
+from typing import List
+from passenger import Passenger
+from driver import Driver
+class RideSharingServiceApp:
+    def __int__(self):
+        self.drivers:List[Driver]=[]
+        self.passengers:List[Passenger]=[]
